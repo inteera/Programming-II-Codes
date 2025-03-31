9cf0dddc90525beaa6aeb2079e6b21808fc394fe
Arda Coskun <inteera@protonmail.com>
2025-03-31 14:23:38 +0300

    To-Do-App initialize

--- SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/example/SpringBootDemo/SpringBootDemoApplication.java
@@ -4,42 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication extends javax.swing.JFrame {
-
-    public SpringBootDemoApplication() {
-        initComponents();
-    }
-    
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    public static void main(String args[]) {
+public class SpringBootDemoApplication {
+     public static void main(String args[]) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
         
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SpringBootDemoApplication().setVisible(true);
-            }
-        });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
 }
